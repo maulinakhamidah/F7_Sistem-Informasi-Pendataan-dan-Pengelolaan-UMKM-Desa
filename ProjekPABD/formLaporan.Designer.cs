54636dcb0af572e90c9127b061a049ffe3f7e87b
maulinakhamidah <maulinakhamidah115@gmail.com>
2026-04-20 05:58:59 +0700

    menghapus event handler label1.Click yang menyebabkan error dan merapikan layout formLaporan

--- ProjekPABD/formLaporan.Designer.cs
+++ ProjekPABD/formLaporan.Designer.cs
@@ -20,5 +20,88 @@
             base.Dispose(disposing);
         }
 
-       
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.dgvLaporan = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLaporan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 60);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(761, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Laporan Sistem Informasi Pendataan dan Pengelolaan UMKM Desa";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // BARIS YANG ERROR (label1.Click) SUDAH DIHAPUS DI SINI
+            // 
+            // dgvLaporan
+            // 
+            this.dgvLaporan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLaporan.Location = new System.Drawing.Point(12, 130);
+            this.dgvLaporan.Name = "dgvLaporan";
+            this.dgvLaporan.RowHeadersWidth = 51;
+            this.dgvLaporan.RowTemplate.Height = 24;
+            this.dgvLaporan.Size = new System.Drawing.Size(776, 210); // Diperlebar agar tidak polos
+            this.dgvLaporan.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 360);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "<- Kembali ke Menu";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(713, 360);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 30);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Logout";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // formLaporan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgvLaporan);
+            this.Controls.Add(this.label1);
+            this.Name = "formLaporan";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen; // Muncul di tengah
+            this.Text = "Laporan Terintegrasi UMKM";
+            this.Load += new System.EventHandler(this.formLaporan_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLaporan)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.DataGridView dgvLaporan;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
 }
